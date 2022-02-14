cbf723f7791dcf0a1cb0ef820910042e53362398
sernik <nickstagman@gmail.com>
2022-02-14 14:36:08 -0500

    continued algX implementation

--- src/AlgX.tsx
+++ src/AlgX.tsx
@@ -71,6 +71,46 @@ class AlgX_Matrix {
     for(let i=0; i<numRows; i++) { this.rows.push(new AlgxNode(i, -1)); }
     for(let i=0; i<numCols; i++) { this.cols.push(new AlgxNode(-1, i, 0)); }
     //initialize links for row and col headers
-    
+    this.rows.forEach((node: AlgxNode, i: number) => {
+      node.right = node;
+      node.left = node;
+      node.down = i < this.rows.length - 1 ? this.rows[i+1] : this.root;
+      node.up = i > 0 ? this.rows[i-1] : this.root;
+    });
+    this.cols.forEach((node: AlgxNode, i: number) => {
+      node.up = node;
+      node.down = node;
+      node.right = i < this.cols.length - 1 ? this.cols[i+1] : this.root;
+      node.left = i > 0 ? this.cols[i-1] : this.root;
+    });
+    //initialize links for root
+    this.root.right = this.cols[0];
+    this.root.left = this.cols[this.cols.length-1];
+    this.root.down = this.rows[0];
+    this.root.up = this.rows[this.rows.length-1];
+  }
+
+  insertRow(coords: Array<[number, number]>): void {
+
+  }
+
+  removeRow(row: number): void {
+
+  }
+
+  cover(node: AlgxNode): void {
+
+  }
+
+  uncover(node: AlgxNode): void {
+
+  }
+
+  algXSearch(): boolean {
+    return false;
+  }
+
+  isEmpty(): boolean {
+    return this.root.right === this.root;
   }
 }
